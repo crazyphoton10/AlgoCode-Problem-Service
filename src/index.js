776ab3330c134c9288e9784323fa6e3aca40c386
Kumar Shantanu <thecrazyphoton@gmail.com>
2024-05-23 20:39:19 +0530

    Feat: Added mongoose db config

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const { PORT } = require("./config/server.config");
 const apiRouter = require("./routes");
 const BaseError = require("./errors/base.error");
 const errorHandler = require("./utils/errorHandler");
+const connectToDb = require("./config/db.config");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -20,6 +22,13 @@ app.get("/ping", (req, res) => {
 //Last middleware if error comes
 app.use(errorHandler);
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server started at PORT ${PORT}`);
+  await connectToDb();
+  console.log("Successfully connected to DB");
+
+  //Dummy code for Testing
+  const Cat = mongoose.model("Cat", { name: String });
+  const kitty = new Cat({ name: "Zildjian" });
+  kitty.save().then(() => console.log("meow"));
 });
